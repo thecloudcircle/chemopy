fe1c4fc0f4370972fc770e82d6fc62b416be9be8
Alex Thomas <alexander.n.thomas@gmail.com>
2021-02-11 23:17:44 -0800

    all ADMET models working

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 00:27:37 2012
+
+@author: orient
+"""
+
+from setuptools import setup, find_packages
+
+packagedata={'pychem': ['html/*','data/*','manual/*']}
+
+
+setup(name = 'chemopy', 
+
+	version = '1.0', 
+	
+	description ="A powerful tool for chemoinformatics study",
+	
+	author = "Dongsheng Cao",
+	
+	author_email = "oriental-cds@163.com",
+	
+	url ="http://cbdd.csu.edu.cn/index",
+	
+	license = "GPL",
+	
+	packages = ['pychem'],
+	
+	package_data=packagedata,
+	
+	scripts = [],
+	
+	py_modules = []
+
+	)
+
